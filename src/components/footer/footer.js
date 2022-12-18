59a0f51bb142b97beaa0ed03fea7a2b7b5357704
manish bisht <manishbisth9711@gmail.com>
2022-12-18 18:46:46 +0530

    Chores : fav icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,34 +5,30 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__main container gap--lg container--center-row">
-        <img src={getImageByKey( "logo")} alt="artsy" />
+        <img src={getImageByKey("logo")} alt="artsy" />
 
         <ul className="footer__links container container--col gap--md">
           <li>Home</li>
-          <li>Marketplace</li>
-          <li>Auctions</li>
-          <li>Drops</li>
-        </ul>
-        <ul className="footer__links container container--col gap--md">
-          <li>Blog</li>
-          <li>Wallets</li>
-          <li>Rates</li>
-          <li>High bids</li>
+          <li>Live Support</li>
         </ul>
         <ul className="footer__links container container--col gap--md">
           <li className="container gap--md">
             <img className="footer__icon" src={getImageByKey("mail")} alt="" />
-            artsystudios@gmail.com
+            Manishbishtdev@gmail.com
           </li>
           <li className="container gap--md">
-            <img className="footer__icon" src={getImageByKey("location")} alt="" /> Lagos,
-            Nigeria.
+            <img
+              className="footer__icon"
+              src={getImageByKey("location")}
+              alt=""
+            />{" "}
+            Lagos, Nigeria.
           </li>
         </ul>
       </div>
 
       <p className="copyright container container--center">
-        Artsystudios © 2022. All Rights Reserved.
+        Manish Bisht © 2022. All Rights Reserved.
       </p>
     </footer>
   );
